80179d2a2e42b9830dd08b038b90e91f5bab41f6
di37 <d.isham.993@gmail.com>
2024-01-03 07:18:30 +0600

    Fourth Commit.

--- vision.py
+++ vision.py
@@ -11,39 +11,7 @@ media_content, media_type = handle_media_upload()
 # Handle JSON file upload for authentication
 api_key = handle_credentials(media_type=media_type)
 
-# input = st.text_input("Input Prompt: ", key="input")
-
-# Allow multiple prompts
-# st.write("Enter a single or multiple prompts.")
-
-# # Initialize session state variables if they don't exist
-# if "input_list" not in st.session_state:
-#     st.session_state.input_list = [""]
-
-
-# # Function to add a new input field
-# def add_input():
-#     st.session_state.input_list.append("")
-
-
-# # Function to remove an input field
-# def remove_input(index):
-#     st.session_state.input_list.pop(index)
-
-
-# # Display the input fields
-# for index, value in enumerate(st.session_state.input_list):
-#     col1, col2 = st.columns([4, 1])
-#     with col1:
-#         st.session_state.input_list[index] = st.text_input(
-#             f"Input Prompt: {index+1}", value=value
-#         )
-#     with col2:
-#         st.button("Remove", key=f"remove_{index}", on_click=remove_input, args=(index,))
-
-
-# # Button to add new input field
-# st.button("Add new input", on_click=add_input)
+# Handle input fields
 prompts = manage_input_fields()
 
 submit = st.button(f"Tell me about the {media_type}")
